99c6e304b7dc3b02f40c2cf83d23f92d9e2b0457
乌云与金边 <1471608219@qq.com>
2021-03-31 10:49:07 +0800

    git test

---
+++ src/main/java/com/test/Test1/App.java
@@ -0,0 +1,15 @@
+package com.test.Test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a = 5;
+        System.out.println( "Hello World!" );
+        System.out.println( "git test." );
+    }
+}
